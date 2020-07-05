d1375d1d7a155a0f4e1641c6cc9f3fffca2cd8d3
Oxygen <ozon0605@gmail.com>
2020-07-05 16:55:52 +0900

    問題の入力方法をテキスト読み込み式に変更 出力方法を変更

--- recursive_function/sudoku.py
+++ recursive_function/sudoku.py
@@ -1,14 +1,17 @@
+# -*- coding: utf-8 -*-
 
+# 数独パズル9×9 ソルバー
+# 手法:再起関数を用いた深さ優先探索
 # ref: https://qiita.com/drken/items/23a4f604fa3f505dd5ad
-# 数独パズル9×9
 
 def printAns(res):
+    print('\nanswer: ')
     for l in res:
-        print(l)
+        print(*l)
 
-def rec(field):
+def solve_sudoku(field):
     
-    # -1のi,jを特定する。
+    # -1のマスを特定する。
     emp_i = -1
     emp_j = -1
     for i in range(9):
@@ -20,7 +23,7 @@ def rec(field):
         if emp_i != -1:
             break
     
-    # base case 空きますゼロ。
+    # 全部埋まったら標準出力する
     if(emp_i == -1) or (emp_j == -1):
         printAns(field)
         return
@@ -50,22 +53,28 @@ def rec(field):
     for i in range(1,10):
         if isAvailable[i]:
             field[emp_i][emp_j] = i
-            rec(field)
+            solve_sudoku(field)
     
-    # 候補が全てダメだったら-1に戻す
+    # 候補が全てダメだったら-1に戻す(重要！)
     field[emp_i][emp_j] = -1
     return
 
 if __name__ == "__main__":
     
     field = [[-1 for j in range(9)] for i in range(9)]
-    for i in range(9):
-        line = input()
-        for j in range(9):
-            l = line[j:j+1]
-            if l == "*":
-                continue
-            else:
-               field[i][j] = int(l)
     
-    rec(field)
+    # 問題読み込み
+    with open('sudoku_mondai.txt','r') as f:
+        print("suudoku puzzle:")
+        for i in range(9):
+            line = f.readline()
+            print(*line, end="")
+            for j in range(9):
+                l = line[j:j+1]
+                if l == "*":
+                    continue
+                else:
+                   field[i][j] = int(l)
+    
+    # ソルバー起動
+    solve_sudoku(field)
